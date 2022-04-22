35da3af784fc35e672dd7a873fc8d66e69c00c62
Murtada Alzubaidi <murtada.alzubaidi@softwire.com>
2022-04-22 15:09:30 +0100

    second refactor

--- javascript-jest/tennis1.js
+++ javascript-jest/tennis1.js
@@ -5,31 +5,28 @@ function getScore(playerOneScore, playerTwoScore) {
         0: "Love",
         1: "Fifteen",
         2: "Thirty",
-        3: "Forty"
-    };
-    const equalScoreNames = {
-        0: "Love-All",
-        1: "Fifteen-All",
-        2: "Thirty-All",
-        3: "Deuce",
-    };
-    const advantageScoreNames = {
-        '1': "Advantage player1",
-        '-1': "Advantage player2",
-        '2': "Win for player1",
-        '-2': "Win for player2",
+        3: "Forty",
+        deuce: "Deuce",
+        advantageAndWin: {
+            "1": "Advantage player1",
+            "-1": "Advantage player2",
+            "2": "Win for player1",
+            "-2": "Win for player2",
+            getScoreName: (playerOneScore, playerTwoScore) => scoreNames.advantageAndWin[Math.max(-2, Math.min(2, playerOneScore - playerTwoScore)).toString()]
+        }
     };
 
     if (playerOneScore === playerTwoScore) {
-        return equalScoreNames[Math.min(playerOneScore, 3)];
+        if (playerOneScore >= 3) { return scoreNames.deuce; }
+
+        return `${scoreNames[playerOneScore]}-All`;
     }
 
     if (playerOneScore >= 4 || playerTwoScore >= 4) {
-        return advantageScoreNames["" + Math.max(-2, Math.min(2, playerOneScore - playerTwoScore))];
+        return scoreNames.advantageAndWin.getScoreName(playerOneScore, playerTwoScore);
     }
 
     return `${scoreNames[playerOneScore]}-${scoreNames[playerTwoScore]}`;
-
 }
 
 module.exports = getScore;
